8c091eb18aaeff89f3523228021a2579ef03738d
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2025-09-11 18:49:10 +0000

    gamon3: update 1.0.6 bottle.

--- Formula/gamon3.rb
+++ Formula/gamon3.rb
@@ -7,8 +7,8 @@ class Gamon3 < Formula
   head "https://github.com/peter-bread/gamon3.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/peter-bread/homebrew-tap/releases/download/gamon3-1.0.5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "9f0d5df893ae0176def4eef154523389e9505c6ec8c480019ac8e46a3b2d7ff0"
+    root_url "https://github.com/peter-bread/homebrew-tap/releases/download/gamon3-1.0.6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9035fe41a82aa392145b731ae36c10b47d6b611f3d52600599d7a427817c354b"
   end
 
   depends_on "go" => :build
