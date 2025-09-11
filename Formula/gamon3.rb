414a6ae58af3bbc263b738707a4e267652b0752c
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2025-09-11 16:45:37 +0100

    gamon3 1.0.5 (new formula)
    Closes #1.
    
    Signed-off-by: Peter Sheehan <145384599+peter-bread@users.noreply.github.com>

---
+++ Formula/gamon3.rb
@@ -0,0 +1,33 @@
+class Gamon3 < Formula
+  desc "Automatically switch GitHub CLI account on `cd`"
+  homepage "https://github.com/peter-bread/gamon3"
+  url "https://github.com/peter-bread/gamon3/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "d2e2f5d8afc4854537924ea4ce85c761799d409d7dcedbb92207e81101cad556"
+  license "MIT"
+  head "https://github.com/peter-bread/gamon3.git", branch: "main"
+
+  depends_on "go" => :build
+  depends_on "gh"
+
+  def install
+    gamon3_version = if build.stable?
+      version.to_s
+    else
+      Utils.safe_popen_read("git", "describe", "--tags", "--dirty").chomp
+    end
+
+    ldflags = %W[
+      -s -w
+      -X main.version=#{gamon3_version}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "-o", "bin/gamon3"
+    bin.install "bin/gamon3"
+
+    generate_completions_from_executable(bin/"gamon3", "completion")
+  end
+
+  test do
+    assert_match "gamon3 version #{version}", shell_output("#{bin}/gamon3 --version")
+  end
+end
